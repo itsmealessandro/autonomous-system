dcd6518973513e39957166c84bd8cc3273681e9c
itsmealessandro <itsmealessandro@outlook.com>
2025-02-05 19:34:25 +0100

    [MQTT] setup done

--- sensors/sens.py
+++ sensors/sens.py
@@ -1,5 +1,14 @@
 import json
 import time
+import paho.mqtt.client as mqtt, time
+
+
+# Creazione del client MQTT
+mqttc = mqtt.Client()
+
+mqttc.connect("mosquitto-container", 1883, 60)
+
+
 
 FILE_PATH = "/simulated_env/env.json"
 # Lettura dal file
@@ -16,10 +25,15 @@ try:
             value_x = data.get("data1", {}).get("value_x", None)
 
             if value_x is not None:
-                print(f"valore  : {value_x}")
+                message= "this is the value_x: "+ str(value_x)
+                mqttc.publish("sens1", message)
+                print(f"published value  : {value_x}")
             else:
                 print("Errore: valore 'value_x' non trovato nel JSON.")
 except FileNotFoundError:
     print(f"Errore: il file {FILE_PATH} non esiste.")
 except json.JSONDecodeError:
     print(f"Errore: il file {FILE_PATH} non è un JSON valido.")
+
+
+mqttc.disconnect()
