30577b0b11c3f89985fea3de7175bf96fb956e27
itsmealessandro <itsmealessandro@outlook.com>
2025-02-20 16:22:32 +0100

    [osgi] tests with jar files

---
+++ osgiTest/1_Osgi/2_simpleOsgiJar/test.java
@@ -0,0 +1,12 @@
+public class test {
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      System.out.println("no input");
+    } else {
+      System.out.println("there are " + args.length + " inputs");
+    }
+
+    System.out.println("hello i'm alive");
+
+  }
+}
