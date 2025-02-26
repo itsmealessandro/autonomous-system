43c81b06b9fb60e7c816b45f6cabadf2beb14d87
itsmealessandro <itsmealessandro@outlook.com>
2025-02-26 18:08:25 +0100

    [osgi] automatic installer done (not generic)

--- osgiTest/1_Osgi/3_automaticBundleInstaller/src/Activator.java
+++ osgiTest/1_Osgi/3_automaticBundleInstaller/src/Activator.java
@@ -1,78 +1,99 @@
 import java.io.File;
-
+import java.io.FileWriter;
+import java.io.IOException;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 
+/*
+ * This class has the goal of installing in osgi the Bundle "testOsgi.jar" if is in the directory defined by the PATH*/
 public class Activator implements BundleActivator {
 
   private static final long INTERVAL = 5000;
-  private static final String BUNDLE = ".";
+  private static final String BUNDLE = "testOsgi.jar";
+  private static final String PATH = "/home/alessandro/Desktop/Università/magistrale/1_anno/as/autonomous-system/osgiTest/1_Osgi/3_automaticBundleInstaller/";
 
-  private final Thread thread = new Thread(new BundleUpdater());
+  private Thread thread;
   private static BundleContext context;
+  private long lastModifiedTime = 0; // Per tenere traccia dell'ultima modifica
 
   static BundleContext getContext() {
     return context;
   }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see
-   * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-   */
+  @Override
   public void start(BundleContext bundleContext) throws Exception {
     Activator.context = bundleContext;
-    thread.start();
+    System.out.println("Bundle started!");
+
+    // Avvia il thread per monitorare il file
+    startBundleUpdater();
+
+    // Scrive nel file di log quando il bundle parte
+    FileWriter hello_file = new FileWriter("BundleFileLog.txt");
+    hello_file.write("I just started");
+    hello_file.close();
   }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see
-   * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-   */
+  @Override
   public void stop(BundleContext bundleContext) throws Exception {
     Activator.context = null;
-    thread.interrupt();
-  }
-
-  protected Bundle findBundleByLocation(String location) {
-    Bundle[] bundles = context.getBundles();
-    for (int i = 0; i < bundles.length; i++) {
-      if (bundles[i].getLocation().equals(location)) {
-        return bundles[i];
-      }
+    if (thread != null) {
+      thread.interrupt();
     }
-    return null;
+    System.out.println("Bundle stopping...");
+
+    // Scrive nel file di log quando il bundle si ferma
+    FileWriter hello_file = new FileWriter("BundleStop.txt");
+    hello_file.write("I just stopped");
+    hello_file.close();
   }
 
-  private class BundleUpdater implements Runnable {
-    public void run() {
+  private void startBundleUpdater() {
+    thread = new Thread(() -> {
       try {
-        File file = new File(BUNDLE);
-        String location = "file:///" + BUNDLE;
+        System.out.println("[DEBUG] Runnable started");
+        String location = PATH + BUNDLE;
+
+        File file = new File(location);
+
         while (!Thread.currentThread().isInterrupted()) {
-          Thread.sleep(INTERVAL);
-          Bundle bundle = findBundleByLocation(location);
-          if (bundle != null && file.exists()) {
-            long bundleModified = bundle.getLastModified();
+          Thread.sleep(INTERVAL); // Dorme per 5 secondi
+
+          System.out.println("Checking for file...");
+          if (file.exists()) {
+            // Verifica se il file è stato modificato
             long fileModified = file.lastModified();
-            if (fileModified > bundleModified) {
-              System.out.println("File is newer, updating.");
-              bundle.update();
+            if (fileModified > lastModifiedTime) {
+              // Se il file è stato modificato, installa il nuovo bundle
+              System.out.println("File is newer, installing the bundle...");
+              installNewBundle(location);
+              lastModifiedTime = fileModified; // Aggiorna il timestamp dell'ultima modifica
+            } else {
+              System.out.println("No change in the file, skipping installation.");
             }
+          } else {
+            System.out.println("Bundle file not found.");
           }
         }
       } catch (InterruptedException e) {
-        System.out.println("I'm going now.");
-      } catch (BundleException e) {
-        System.out.println("Error updating bundle.");
-        e.printStackTrace();
+        System.out.println("[DEBUG] Thread interrupted.");
       }
-*/
+    });
+    thread.start();
+  }
+
+  private void installNewBundle(String location) {
+    try {
+      System.out.println("Installing bundle from location: " + location);
+      // Installa il nuovo bundle
+      Bundle bundle = context.installBundle("file:///" + location); // Aggiungi 'file:///' per il percorso locale
+      System.out.println("Bundle installed: " + bundle.getSymbolicName());
+      bundle.start(); // Puoi avviare il bundle subito dopo l'installazione
+    } catch (BundleException e) {
+      System.out.println("Error installing bundle from " + location);
+      e.printStackTrace();
     }
   }
 }
